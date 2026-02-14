dc1d6a79ed91a32d9c8df478e703ce366ae3395c
Bikash Kumar Gupta <43280465+bikashkrgupta@users.noreply.github.com>
2026-02-14 12:16:38 +0530

    Update dependency_service.py

--- services/dependency_service.py
+++ services/dependency_service.py
@@ -1,1 +1,22 @@
+import re
+
+def parse_dax_dependencies(dax_expressions):
+
+    used_fields = set()
+    pattern = r"([A-Za-z0-9_ ]+)\[([A-Za-z0-9_ ]+)\]"
+
+    for dax in dax_expressions:
+
+        if isinstance(dax, list):
+            dax = " ".join(dax)
+
+        if not isinstance(dax, str):
+            continue
+
+        matches = re.findall(pattern, dax)
+
+        for table, column in matches:
+            used_fields.add(f"{table.strip()}[{column.strip()}]")
+
+    return used_fields
 
