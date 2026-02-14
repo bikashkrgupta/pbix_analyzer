9765d615b41d9b654ff5aaea0eb278c4a5b9dadc
Bikash Kumar Gupta <43280465+bikashkrgupta@users.noreply.github.com>
2026-02-14 12:21:05 +0530

    Update constants.py

--- core/constants.py
+++ core/constants.py
@@ -1,1 +1,20 @@
+# File & Folder Names
+SCHEMA_FILE_NAME = "DataModelSchema"
+LAYOUT_FILE_PATH = "Report/Layout"
+
+# Encoding Priority
+SUPPORTED_ENCODINGS = [
+    "utf-16-le",
+    "utf-16-be",
+    "utf-8"
+]
+
+# System Date Table Annotations
+SYSTEM_DATE_ANNOTATIONS = [
+    "__PBI_TemplateDateTable",
+    "__PBI_LocalDateTable"
+]
+
+# DAX Regex Pattern
+DAX_DEPENDENCY_PATTERN = r"([A-Za-z0-9_ ]+)\[([A-Za-z0-9_ ]+)\]"
 
