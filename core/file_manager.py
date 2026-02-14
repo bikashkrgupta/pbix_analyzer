a3e75d28463f90867919acee3daf4ff2f64f8576
Bikash Kumar Gupta <43280465+bikashkrgupta@users.noreply.github.com>
2026-02-14 12:14:23 +0530

    Update file_manager.py

--- core/file_manager.py
+++ core/file_manager.py
@@ -1,1 +1,25 @@
+import os
+import shutil
+import zipfile
+
+def prepare_workspace():
+    base_path = os.path.join(os.getcwd(), "workspace")
+
+    if os.path.exists(base_path):
+        shutil.rmtree(base_path)
+
+    os.makedirs(base_path, exist_ok=True)
+    return base_path
+
+
+def extract_zip(uploaded_file, base_path):
+    file_path = os.path.join(base_path, uploaded_file.name)
+
+    with open(file_path, "wb") as f:
+        f.write(uploaded_file.getbuffer())
+
+    with zipfile.ZipFile(file_path, 'r') as zip_ref:
+        zip_ref.extractall(base_path)
+
+    return base_path
 
