a9bc8c24ceae5f77319ba098b7effc2e0bebff85
Bikash Kumar Gupta <43280465+bikashkrgupta@users.noreply.github.com>
2026-02-14 12:20:46 +0530

    Update analysis_model.py

--- models/analysis_model.py
+++ models/analysis_model.py
@@ -1,1 +1,22 @@
+class AnalysisResult:
+
+    def __init__(self):
+        self.tables = set()
+        self.columns = set()
+        self.measures = set()
+
+        self.used_columns = set()
+        self.unused_columns = set()
+
+        self.used_measures = set()
+        self.unused_measures = set()
+
+    def compute_summary(self):
+        return {
+            "Total Tables": len(self.tables),
+            "Total Columns": len(self.columns),
+            "Total Measures": len(self.measures),
+            "Unused Columns": len(self.unused_columns),
+            "Unused Measures": len(self.unused_measures)
+        }
 
